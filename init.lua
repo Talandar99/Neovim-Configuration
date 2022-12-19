6c96c33bb7e28f2a8b5ea128957832b9400bb684
Grzegorz <grzegorzc66@gmail.com>
2022-12-19 20:27:40 +0100

    removed unused lines

--- init.lua
+++ init.lua
@@ -49,24 +49,11 @@ require('packer').startup(function(use)
 	use "lukas-reineke/indent-blankline.nvim" -- indent blankline
 	use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }-- Telescope
 	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-  	use { -- Highlight, edit, and navigate code
-    	'nvim-treesitter/nvim-treesitter',
-    	run = function()
-      		pcall(require('nvim-treesitter.install').update { with_sync = true })
-    	end,}
-  	use { -- Additional text objects via treesitter
-    	'nvim-treesitter/nvim-treesitter-textobjects',
-    	after = 'nvim-treesitter',}
-  	if is_bootstrap then
-   		require('packer').sync()
-  	end
+  	use {'nvim-treesitter/nvim-treesitter',run = function()  -- Highlight, edit, and navigate code
+      	pcall(require('nvim-treesitter.install').update { with_sync = true }) end,}
+  	use {'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter',} -- Additional text objects via treesitter
+  	if is_bootstrap then require('packer').sync() end
 end)
-
-
-require("indent_blankline").setup {
-	show_current_context = true,
-    show_current_context_start = true,
-}
 if is_bootstrap then
   print '=================================='
   print '    useins are being installed'
@@ -75,7 +62,6 @@ if is_bootstrap then
   print '=================================='
   return
 end
-
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
   command = 'source <afile> | PackerCompile',
@@ -83,6 +69,10 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = vim.fn.expand '$MYVIMRC',
 })
 
+require("indent_blankline").setup {
+	show_current_context = true,
+	show_current_context_start = true,
+}
 require('initial_setup') --local
 require('key_mappings') --local
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())

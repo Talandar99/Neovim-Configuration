995c3fbbe65218128bdb1f0e4467d0844efd79e6
Talandar <talandar66@protonmail.com>
2023-03-29 11:52:29 +0200

    changed formating, and lsp_servers replaced deprecated formating_sync with format improved rust lsp

--- init.lua
+++ init.lua
@@ -20,7 +20,6 @@ require('packer').startup(function(use)
 	use 'kyazdani42/nvim-web-devicons' --icons in your statusline
 	use 'nvim-telescope/telescope-file-browser.nvim'
 	use 'startup-nvim/startup.nvim'   --startup
-	use 'ThePrimeagen/harpoon'        --Harpoon
 	use {                             -- LSP + MASON
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
@@ -57,6 +56,7 @@ require('packer').startup(function(use)
 		pcall(require('nvim-treesitter.install').update { with_sync = true })
 	end, }
 	use { 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter', } -- Additional text objects via treesitter
+	use 'ThePrimeagen/harpoon'                                                       --Harpoon
 	use({
 		"glepnir/lspsaga.nvim",
 		branch = "main",
@@ -111,7 +111,7 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 	pattern = vim.fn.expand '$MYVIMRC',
 })
 -- Autoformat on save
-vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 require("indent_blankline").setup {
 	show_current_context = true,
 	show_current_context_start = true,
@@ -136,29 +136,29 @@ require("mason-lspconfig").setup({
 })
 require("telescope").load_extension "file_browser"
 require("telescope").load_extension "harpoon"
-require("startup").setup({ theme = "talandar" })   -- put theme name here
-require('lsp_servers')                             -- local
-require('cmpsetup')                                -- local
-require('lsp_floating_window_border')              --local
-require('treesitter_config')                       -- local
-require('tokyonightsetup')                         -- local
+require("startup").setup({ theme = "talandar" })    -- put theme name here
+require('lsp_servers')                              -- local
+require('cmpsetup')                                 -- local
+require('lsp_floating_window_border')               --local
+require('treesitter_config')                        -- local
+require('tokyonightsetup')                          -- local
 require("presence"):setup({
-	auto_update         = true,                      -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
-	neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
-	main_image          = "neovim",                  -- Main image display (either "neovim" or "file")
-	client_id           = "793271441293967371",      -- Use your own Discord application client id (not recommended)
-	log_level           = nil,                       -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-	debounce_timeout    = 10,                        -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-	enable_line_number  = false,                     -- Displays the current line number instead of the current project
-	blacklist           = {},                        -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-	buttons             = true,                      -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
-	file_assets         = {},                        -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-	show_time           = true,                      -- Show the timer
-	editing_text        = "Editing %s",              -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-	file_explorer_text  = "Browsing %s",             -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
-	git_commit_text     = "Committing changes",      -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-	plugin_manager_text = "Managing plugins",        -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-	reading_text        = "Reading %s",              -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-	workspace_text      = "Working on %s",           -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-	line_number_text    = "Line %s out of %s",       -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+	auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+	neovim_image_text   = "The Only True Text Editor", -- Text displayed when hovered over the Neovim image
+	main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
+	client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
+	log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+	debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+	enable_line_number  = false,                      -- Displays the current line number instead of the current project
+	blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+	buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+	file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+	show_time           = true,                       -- Show the timer
+	editing_text        = "Editing %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+	file_explorer_text  = "Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+	git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+	plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+	reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+	workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+	line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
 })

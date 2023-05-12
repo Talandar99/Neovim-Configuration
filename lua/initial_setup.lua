85c2a750e59cab12723704477a741ad56f8b0c47
Talandar <talandar66@protonmail.com>
2023-05-12 20:46:22 +0200

    translated remaining vimscript to lua

--- lua/initial_setup.lua
+++ lua/initial_setup.lua
@@ -1,51 +1,49 @@
 vim.opt.incsearch = true
 vim.opt.scrolloff = 8
-vim.cmd(
-	[[
-set clipboard=unnamedplus
-set completeopt=menu,menuone,noselect
-set relativenumber
-set number
-set signcolumn=yes
-syntax on
-set t_Co=256
-set cursorline
-set splitbelow
-set encoding=UTF-8
-set noerrorbells                                              	
-set nowrap
-set nobackup
-set nowritebackup
-set noswapfile
-set nohlsearch
-set smartcase
-set undodir=~/.nvim-data/backup
-set undofile
-set incsearch
-set tabstop=4
-set softtabstop=0 noexpandtab
-set shiftwidth=4
-set backspace=indent,eol,start
-set updatetime=50
-set termguicolors
-let g:rust_cargo_check_all_targets = 1
-let g:rustfmt_autosave = 1
-let g:dart_format_on_save = 1
-let g:dartfmt_options = ['--fix', '--line-length 120']
-let g:signify_sign_add               = '+'
-let g:signify_sign_delete            = '_'
-let g:signify_sign_delete_first_line = '‾'
-let g:signify_sign_change            = '~'
-let g:signify_disable_by_default = 1 
-let g:signify_sign_show_count = 0
-let g:signify_sign_show_text = 1
-set nocompatible
-filetype plugin on
-let g:lightline = {'colorscheme': 'tokyonight'}
-let g:LanguageClient_serverCommands = {
-    \ 'sh': ['bash-language-server', 'start']
-    \ }
-let g:mix_format_on_save = 1
-let g:mix_format_silent_errors = 1
-]]
-)
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.signcolumn = 'yes'
+vim.opt.syntax = 'on'
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.encoding = 'UTF-8'
+vim.opt.errorbells = false
+vim.opt.wrap = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.hlsearch = false
+vim.opt.smartcase = true
+vim.opt.undodir = '~/.nvim-data/backup'
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.backspace = {'indent', 'eol', 'start'}
+vim.opt.updatetime = 50
+vim.opt.termguicolors = true
+
+vim.g.rust_cargo_check_all_targets = 1
+vim.g.rustfmt_autosave = 1
+vim.g.dart_format_on_save = 1
+vim.g.dartfmt_options = {'--fix', '--line-length 120'}
+vim.g.signify_sign_add = '+'
+vim.g.signify_sign_delete = '_'
+vim.g.signify_sign_delete_first_line = '‾'
+vim.g.signify_sign_change = '~'
+vim.g.signify_disable_by_default = 1
+vim.g.signify_sign_show_count = 0
+vim.g.signify_sign_show_text = 1
+
+vim.o.compatible = false
+vim.cmd('filetype plugin on')
+vim.g.lightline = {colorscheme = 'tokyonight'}
+vim.g.LanguageClient_serverCommands = {
+  sh = {'bash-language-server', 'start'}
+}
+vim.g.mix_format_on_save = 1
+vim.g.mix_format_silent_errors = 1
